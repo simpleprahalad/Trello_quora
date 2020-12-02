e810e97487ec21b2510bc5a32ff834e752dd36bf
Deepak-dekumark <deepakkrhp@gmail.com>
2020-12-02 23:09:51 +0530

    Added answer controller,DAO,Service and Entity

---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -0,0 +1,15 @@
+package com.upgrad.quora.service.dao;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class AnswerDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+}
+
